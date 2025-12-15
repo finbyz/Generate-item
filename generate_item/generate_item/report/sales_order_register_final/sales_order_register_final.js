52e898ae33eae18270fe15529bf6a57df3927a91
Finbyz <info@finbyz.tech>
2025-12-15 12:18:51 +0000

    Sales Order Register Final Report

---
+++ generate_item/generate_item/report/sales_order_register_final/sales_order_register_final.js
@@ -0,0 +1,74 @@
+// Copyright (c) 2025, Finbyz and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Order Register Final"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+			"reqd": 1,
+			"width": "100",
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "100",
+			"on_change": () => frappe.query_report.refresh()
+		},		
+		{
+			"fieldname": "sales_order",
+			"label": __("Sales Order"),
+			"fieldtype": "Link",
+			"options": "Sales Order",
+			"width": "150",
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+			"width": "150",
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "item_code",
+			"label": __("Item Code"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"width": "150",
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "batch_no",
+			"label": __("Batch No"),
+			"fieldtype": "Link",
+			"options": "Batch",
+			"width": "150",
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "branch",
+			"label": __("Branch"),
+			"fieldtype": "Select",
+			"options": ["","Sanand", "Rabale","Nandikoor"],
+			"width": "100",
+			"reqd": 1,
+			"on_change": () => frappe.query_report.refresh()
+		},
+		{
+			"fieldname": "status",
+			"label": __("Order Status"),
+			"fieldtype": "Select",
+			"options": ["", "To Deliver and Bill", "To Deliver", "To Bill", "Closed", "Cancelled"],
+			"width": "150",
+			"on_change": () => frappe.query_report.refresh()
+		}
+	]
+};
