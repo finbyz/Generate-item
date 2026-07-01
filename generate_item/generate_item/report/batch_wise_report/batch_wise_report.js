90d0d95ff492cccdf9cbcece80409fcf9983e7f3
Finbyz <info@finbyz.tech>
2026-07-01 07:21:45 +0000

    batch wise report default value changes --RP

--- generate_item/generate_item/report/batch_wise_report/batch_wise_report.js
+++ generate_item/generate_item/report/batch_wise_report/batch_wise_report.js
@@ -6,19 +6,21 @@ frappe.query_reports["Batch Wise Report"] = {
 		{
 			fieldname: "from_date",
 			label: "From Date",
-			fieldtype: "Date"
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
 		},
 		{
 			fieldname: "to_date",
 			label: "To Date",
-			fieldtype: "Date"
+			fieldtype: "Date",
+			default: frappe.datetime.get_today()
 		},
 		{
             "label": __("Branch"),
             "fieldname": "branch",
             "fieldtype": "Link",
             "options": "Branch",
-            "default": " ",
+            "default": "Sanand",
             "reqd":1,
             "placeholder":"Branch"
         },
