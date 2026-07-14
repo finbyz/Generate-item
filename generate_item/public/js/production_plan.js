166f21e9e69c19bfeae6f8a38c9be0d682bbad37
Ritik Patwa <ritik.patwa@finbyz.tech>
2026-07-14 07:34:08 +0000

    pp changes --DM

--- generate_item/public/js/production_plan.js
+++ generate_item/public/js/production_plan.js
@@ -241,11 +241,11 @@ frappe.ui.form.on('Production Plan', {
     refresh: function (frm) {
         const UPDATE_GROUP = __("Update");
         if (frm.doc.docstatus === 1  ) {
-             if (frm.doc.bom_modification || frm.doc.sales_order_modification) {
+    
         add_update_work_orders_button(frm, UPDATE_GROUP);
         add_get_update_button(frm, UPDATE_GROUP);
         add_create_material_request_button(frm, UPDATE_GROUP);
-    }
+    
            
         
         }
