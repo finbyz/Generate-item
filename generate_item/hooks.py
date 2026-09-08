6814deb080b5345fa50ece34af8dfede6761d85a
Ritik Patwa <ritik.patwa@finbyz.tech>
2026-09-08 10:49:54 +0000

    hide title field in all doctype --DM

--- generate_item/hooks.py
+++ generate_item/hooks.py
@@ -33,7 +33,8 @@ app_license = "mit"
 
 app_include_js = [
     "/assets/generate_item/js/report_export.js?v=6.0",
-     "/assets/generate_item/js/login_redirect.js?v=1.4"
+     "/assets/generate_item/js/login_redirect.js?v=1.4",
+     "/assets/generate_item/js/hide_title.js"
 ]
 # --- Login Redirect feature --------------------------------------------------
 
